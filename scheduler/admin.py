e9e1c5bc6ae04ba6260b7666bf50b2adbdfa4eef
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-08-09 11:18:13 -0400

    tweaks in server

---
+++ scheduler/admin.py
@@ -0,0 +1,26 @@
+import nested_admin
+from django.contrib import admin
+from scheduler.models import SocialSchedule, MediaContentSource,  MediaContent, MediaContentImage
+
+
+class SocialScheduleAdmin(admin.ModelAdmin):
+    list_display = ('source', 'social_media', 'social_media_channel', 'schedule', 'last_run')
+
+
+class MediaContentImageInline(nested_admin.NestedTabularInline):
+    model = MediaContentImage
+    extra = 1
+
+
+class MediaContentInline(nested_admin.NestedStackedInline):
+    model = MediaContent
+    inlines = [MediaContentImageInline]
+    extra = 1
+
+
+@admin.register(MediaContentSource)
+class MediaContentSourceAdmin(nested_admin.NestedModelAdmin):
+    inlines = [MediaContentInline]
+    list_display = ('id', 'name')
+
+admin.site.register(SocialSchedule, SocialScheduleAdmin)
